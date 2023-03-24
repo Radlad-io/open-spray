b9faf9a950ebf92222f6ebd2b90d0f1cb8aa67ee
Kevin Merinsky <kevinmerinsky@gmail.com>
2023-03-23 22:02:32 -0400

    Added button and led to prototype

--- client/start.py
+++ client/start.py
@@ -1,4 +1,5 @@
 from time import sleep
+from machine import Pin
 import modules.secrets as secrets
 from modules.display import Display
 from modules.network import Network
@@ -8,6 +9,8 @@ SSID=secrets.SSID
 PWD=secrets.PWD
 ENDPOINT=secrets.ENDPOINT
 PORT = secrets.PORT
+led = Pin(4, Pin.OUT)
+button = Pin(5, Pin.IN, Pin.PULL_UP)
 
 display = Display()
 network = Network(SSID, PWD)
@@ -45,3 +48,22 @@ def rotary_changed(change):
     display.home_screen(val, ip)
         
 rotary.add_handler(rotary_changed)
+
+def get_button():
+    return not button.value()
+
+def button_press_function():
+    led.value(1)
+    print('LED ON')
+
+def button_released_function():
+    led.value(0)
+    print('LED OFF')
+    
+while True:
+    if get_button() == 1:
+        button_press_function()
+    else:
+        button_released_function()
+    led.value(0)
+    sleep(0.5)
