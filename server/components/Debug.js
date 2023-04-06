0673ca2672d7550a13290d834ce7697e96739d0f
Kevin Merinsky <kevinmerinsky@gmail.com>
2023-04-06 16:53:11 -0400

    Debug color panel

--- server/components/Debug.js
+++ server/components/Debug.js
@@ -1,10 +1,12 @@
 import { Pane } from "tweakpane";
 import * as EssentialsPlugin from "@tweakpane/plugin-essentials";
+import State from "./State";
 import Experience from "./Experience";
 
 export default class Debug {
   constructor() {
     this.experience = new Experience();
+    this.state = new State();
     this.environment = import.meta.env.MODE;
     this.active = window.location.hash === "#debug";
 
@@ -26,6 +28,50 @@ export default class Debug {
       this.clearBtn.on("click", () => {
         this.experience.clear();
       });
+
+      this.addColorControls(
+        this.state.r.get(),
+        this.state.g.get(),
+        this.state.b.get(),
+        this.state.a.get()
+      );
     }
+  }
+  addColorControls(r, g, b, a) {
+    this.PARAMS = {
+      key: {
+        r,
+        g,
+        b,
+        a,
+      },
+    };
+    const colorControl = this.pane
+      .addInput(this.PARAMS, "key", {
+        picker: "inline",
+        color: { alpha: true },
+        expanded: true,
+      })
+      .on("change", (e) => {
+        this.state.r.set(Math.floor(e.value.r));
+        this.state.g.set(Math.floor(e.value.g));
+        this.state.b.set(Math.floor(e.value.b));
+        this.state.a.set(e.value.a);
+        this.state.color.set([
+          this.state.r.get(),
+          this.state.g.get(),
+          this.state.b.get(),
+          this.state.a.get(),
+        ]);
+
+        // console.log(
+        //   this.state.r.get(),
+        //   this.state.g.get(),
+        //   this.state.b.get(),
+        //   this.state.a.get()
+        // );
+
+        console.log(this.state.color.get());
+      });
   }
 }
